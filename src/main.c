392c5f186d7af652e71f40df54ff69e07f2c4547
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-11-28 00:11:16 +0900

    improve : malloc all struct

--- src/main.c
+++ src/main.c
@@ -6,19 +6,34 @@
 /*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 02:08:05 by hiroaki           #+#    #+#             */
-/*   Updated: 2022/11/26 22:01:54 by hiroaki          ###   ########.fr       */
+/*   Updated: 2022/11/28 00:09:35 by hiroaki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-void	fdf_exit(t_data *d, t_map *m, char *errmsg)
+void	free_all_struct(t_data *d)
 {
-	free(m->line);
-	free_all_element((void **)m->coord, m->alloccnt);
-	free_all_element((void **)m->color, m->alloccnt);
-	free_all_element((void **)m->elem, m->width);
-	free_all_element((void **)m->info, m->infocnt);
+	if (!d)
+		return ;
+	free(d->mx);
+	free(d->mlx);
+	free(d);
+}
+
+void	free_all_array(t_matrix *mx)
+{
+	free(mx->line);
+	free_all_element((void **)mx->coord, mx->alloccnt);
+	free_all_element((void **)mx->color_code, mx->alloccnt);
+	free_all_element((void **)mx->elem, mx->width);
+	free_all_element((void **)mx->coord_info, mx->coord_infocnt);
+}
+
+void	fdf_exit(t_data *d, char *errmsg)
+{
+	free_all_array(d->mx);
+	free_all_struct(d);
 	if (errmsg)
 	{
 		ft_dprintf(STD_ERROR, "%s\n", errmsg);
@@ -30,107 +45,108 @@ void	fdf_exit(t_data *d, t_map *m, char *errmsg)
 void	check_argc(t_data *d, int argc)
 {
 	if (argc < 2)
-		fdf_exit(d, &d->m, "Invalid argument.");
+		fdf_exit(d, "Invalid argument.");
 }
 
 void	init_fd(t_data *d, int *fd, char *filename)
 {
 	*fd = open(filename, O_RDONLY);
 	if (fd <= 0)
-		fdf_exit(d, &d->m, "Failed to open file.");
+		fdf_exit(d, "Failed to open file.");
 }
 
-void	get_coord(t_data *d, t_map *m, int i, int j)
+void	get_coord(t_data *d, t_matrix *mx, int i, int j)
 {
 	bool	ok;
 
-	m->coord[i][j] = ft_atoi_base(m->info[0], 10, &ok);
+	mx->coord[i][j] = ft_atoi_base(mx->coord_info[0], 10, &ok);
 	if (!ok)
-		fdf_exit(d, &d->m, "Invalid map coord.");
+		fdf_exit(d, "Invalid map coord.");
 }
 
-void	get_color_code(t_data *d, t_map *m, int i, int j)
+void	get_color_code(t_data *d, t_matrix *mx, int i, int j)
 {
 	bool	ok;
 
-	if (m->infocnt == 1)
-		m->color[i][j] = ft_atoi_base(DEFAULT_COLOR, 16, &ok);
-	if (ok && m->infocnt == 2)
-		m->color[i][j] = ft_atoi_base(d->m.info[1], 16, &ok);
-	if (!ok)
-		fdf_exit(d, &d->m, "Invalid color code.");
+	ok = false;
+	if (mx->coord_infocnt == 1)
+		mx->color_code[i][j] = -1;
+	if (mx->coord_infocnt == 2)
+		mx->color_code[i][j] = ft_atoi_base(mx->coord_info[1], 16, &ok);
+	if (mx->coord_infocnt != 1 && !ok)
+		fdf_exit(d, "Invalid color code.");
 }
 
-void	get_coord_info(t_data *d, t_map *m, char *elem)
+void	get_coord_info(t_data *d, t_matrix *mx, char *elem)
 {
-	m->info = ft_split(elem, ',', &m->infocnt);
-	if (!m->info || m->infocnt > 2)
-		fdf_exit(d, &d->m, "Failed to read map.");
+	mx->coord_info = ft_split(elem, ',', &mx->coord_infocnt);
+	if (!mx->coord_info || mx->coord_infocnt > 2)
+		fdf_exit(d, "Failed to read map.");
 }
 
-void	parse_line(t_data *d, t_map *m, int i)
+void	parse_line(t_data *d, t_matrix *mx, int i)
 {
 	int	j;
 
 	j = -1;
-	while (m->elem[++j])
+	while (mx->elem[++j])
 	{
-		get_coord_info(d, m, m->elem[j]);
-		get_coord(d, m, i, j);
-		get_color_code(d, m, i, j);
-		free_all_element((void **)d->m.info, m->infocnt);
-		d->m.info = NULL;
+		get_coord_info(d, mx, mx->elem[j]);
+		get_coord(d, mx, i, j);
+		get_color_code(d, mx, i, j);
+		free_all_element((void **)mx->coord_info, mx->coord_infocnt);
+		mx->coord_info = NULL;
 	}
 }
 
-void	alloc_arr(t_data *d, t_map *m)
+void	alloc_arr(t_data *d, t_matrix *mx)
 {
 	int	i;
 
-	m->alloccnt = 0;
-	m->coord = (int **)malloc(m->height * sizeof(int *));
-	m->color = (int **)malloc(m->height * sizeof(int *));
-	if (!m->coord || !m->color)
-		fdf_exit(d, m, "Malloc failure");
+	mx->alloccnt = 0;
+	mx->coord = (int **)malloc(mx->height * sizeof(int *));
+	mx->color_code = (int **)malloc(mx->height * sizeof(int *));
+	if (!mx->coord || !mx->color_code)
+		fdf_exit(d, "Malloc failure");
 	i = -1;
-	while (++i < m->height)
+	while (++i < mx->height)
 	{
-		m->coord[i] = (int *)malloc(m->width * sizeof(int));
-		m->color[i] = (int *)malloc(m->width * sizeof(int));
-		if (!m->coord[i] || !m->color[i])
-			fdf_exit(d, m, "Malloc failure");
-		m->alloccnt++;
+		mx->coord[i] = (int *)malloc(mx->width * sizeof(int));
+		mx->color_code[i] = (int *)malloc(mx->width * sizeof(int));
+		if (!mx->coord[i] || !mx->color_code[i])
+			fdf_exit(d, "Malloc failure");
+		mx->alloccnt++;
 	}
 }
 
-void	get_height(t_data *d, t_map *m, int *signal, int fd)
+void	get_height(t_data *d, t_matrix *mx, int *signal, int fd)
 {
-	m->line = get_next_line(fd, signal);
+	mx->line = get_next_line(fd, signal);
 	if (*signal == END_OF_FILE)
 	{
-		free(m->line);
-		m->line = NULL;
+		free(mx->line);
+		mx->line = NULL;
 		return ;
 	}
-	if (!m->line)
-		fdf_exit(d, m, "Failed to read map");
-	m->height++;
+	if (!mx->line)
+		fdf_exit(d, "Failed to read map");
+	mx->height++;
 }
 
-void	get_width(t_data *d, t_map *m)
+void	get_width(t_data *d, t_matrix *mx)
 {
 	int	width;
 
-	ft_split(m->line, ' ', &width);
-	free(m->line);
-	m->line = NULL;
-	if (!m->width)
-		m->width = width;
-	if (!width || m->width != width)
-		fdf_exit(d, m, "Invalid width in map.");
+	ft_split(mx->line, ' ', &width);
+	free(mx->line);
+	mx->line = NULL;
+	if (!mx->width)
+		mx->width = width;
+	if (!width || mx->width != width)
+		fdf_exit(d, "Invalid width in map.");
 }
 
-void	get_range(t_data *d, t_map *m, char *filename)
+void	get_range(t_data *d, char *filename)
 {
 	int	fd;
 	int	signal;
@@ -138,166 +154,263 @@ void	get_range(t_data *d, t_map *m, char *filename)
 	init_fd(d, &fd, filename);
 	while (1)
 	{
-		get_height(d, m, &signal, fd);
+		get_height(d, d->mx, &signal, fd);
 		if (signal == END_OF_FILE)
 			return ;
-		get_width(d, m);
+		get_width(d, d->mx);
 	}
 	close(fd);
 }
 
-void	get_matrix(t_data *d, t_map *m, char *filename)
+void	init_matrix(t_data *d, t_matrix *mx, char *filename)
 {
-	int	fd;
 	int	i;
+	int	fd;
 	int	signal;
 
 	init_fd(d, &fd, filename);
 	i = -1;
 	while (1)
 	{
-		m->line = ft_strtrim(get_next_line(fd, &signal), "\n");
+		mx->line = ft_strtrim(get_next_line(fd, &signal), "\n");
 		if (signal == END_OF_FILE)
 			return ;
-		m->elem = ft_split(m->line, ' ', &m->width);
-		parse_line(d, m, ++i);
-		if (!m->elem)
-			fdf_exit(d, &d->m, "Failed to read map");
-		free(m->line);
+		mx->elem = ft_split(mx->line, ' ', &mx->width);
+		parse_line(d, mx, ++i);
+		if (!mx->elem)
+			fdf_exit(d, "Failed to read map");
+		free(mx->line);
 	}
 	close(fd);
 }
 
-void	get_map(t_data *d, char *filename)
+void	get_matrix(t_data *d, char *filename)
 {
-	int		fd;
+	int	fd;
 
-	get_range(d, &d->m, filename);
-	alloc_arr(d, &d->m);
-	get_matrix(d, &d->m, filename);
+	get_range(d, filename);
+	alloc_arr(d, d->mx);
+	init_matrix(d, d->mx, filename);
 }
 
-int ft_abs(float diff)
-{
-	if (diff < 0)
-		return (-diff);
-	return (diff);
-}
-
-int	ft_max(float a, float b)
-{
-	if (a < b)
-		return (b);
-	return (a);
-}
-
-void	isometric(float *x, float *y, int z)
-{
-	*x = (*x - *y) * cos(0.8);
-	*y = (*x + *y) * sin(0.8) - z;
-}
-
-//void	bresenham_algo(t_data *d, float x, float y, float x1, float y1)
+//int ft_abs(double diff)
+//{
+//	if (diff < 0)
+//		return (-diff);
+//	return (diff);
+//}
+//
+//int	ft_max(double a, double b)
+//{
+//	if (a < b)
+//		return (b);
+//	return (a);
+//}
+//
+//void	isometric(double *x, double *y, int z)
+//{
+//	double	prev_x;
+//	double	prev_y;
+//
+//	prev_x = *x;
+//	prev_y = *y;
+//	*x = (prev_x - prev_y) * cos(0.8);
+//	*y = (prev_x + prev_y) * sin(0.8) - z;
+//}
+//
+//void	rotate_x(double *y, double *z, double alpha)
+//{
+//	double	prev_y;
+//
+//	prev_y = *y;
+//	*y = prev_y * cos(alpha) + *z * sin(alpha);
+//	*z = -prev_y * sin(alpha) + *z * cos(alpha);
+//}
+//
+//void	rotate_y(double *x, double *z, double beta)
+//{
+//	double	prev_x;
+//
+//	prev_x = *x;
+//	*x = prev_x * cos(beta) + *z * sin(beta);
+//	*z = -prev_x * sin(beta) + *z * cos(beta);
+//}
+//
+//void	rotate_z(double *x, double *y, double gamma)
+//{
+//	double	prev_x;
+//	double	prev_y;
+//
+//	prev_x = *x;
+//	prev_y = *y;
+//	*x = prev_x * cos(gamma) - prev_y * sin(gamma);
+//	*y = prev_x * sin(gamma) - prev_y * cos(gamma);
+//}
+//
+//void	default_locate(double *x, double *y, double *x1, double *y1)
+//{
+//	double	x_locate;
+//	double	y_locate;
+//
+//	x_locate = 330;
+//	y_locate = 350;
+//	*x += x_locate;
+//	*x1 += x_locate;
+//	*y += y_locate;
+//	*y1 += y_locate;
+//}
+//
+//void	settings(t_data *d, double *x, double *y, double *x1, double *y1)
+//{
+//	double		z;
+//	double		z1;
+//
+//	z = d->m.coord[(int)*y][(int)*x];
+//	z1 = d->m.coord[(int)*y1][(int)*x1];
+//	d->m.color_code = d->m.color[(int)*y][(int)*x];
+//	*x *= d->m.zoom;
+//	*y *= d->m.zoom;
+//	*x1 *= d->m.zoom;
+//	*y1 *= d->m.zoom;
+//	rotate_x(y, &z, 0);
+//	rotate_y(x, &z, 0);
+//	rotate_z(x, y, 0);
+//	*x += (WIDTH - MENU_WIDTH) / 2 + MENU_WIDTH;
+//	*y += (HEIGHT + d->m.height * d->m.zoom) / 2;
+//	//isometric(x, y, z);
+//	//isometric(x1, y1, z1);
+//	//default_locate(x, y, x1, y1);
+//}
+//
+//#include <stdio.h>
+//void	bresenham_algo(t_data *d, double x, double y, double x1, double y1)
 //{
 //	int		max;
-//	float	x_step;
-//	float	y_step;
-//	int		z;
-//	int		z1;
+//	double	x_step;
+//	double	y_step;
 //
-//	z = d->m.coord[(int)x + (int)y * d->m.width];
-//	z1 = d->m.coord[(int)x1 + (int)y1 * d->m.width];
-//	//x *= d->m.zoom;
-//	//y *= d->m.zoom;
-//	//x1 *= d->m.zoom;
-//	//y1 *= d->m.zoom;
-//	//z *= d->m.zoom;
-//	//z1 *= d->m.zoom;
-//	//isometric(&x, &y, z);
-//	//isometric(&x1, &y1, z1);
-//	//coord_update(d, &x, &y, &x1, &y1);
-//	//x += 150;
-//	//y += 150;
-//	//x1 += 150;
-//	//y1 += 150;
-//	//x_step = ft_abs(x1 - x);
-//	//y_step = ft_abs(y1 - y);
-//	//max = ft_max(x_step, y_step);
-//	//x_step /= max;
-//	//y_step /= max;
-//	//while ((int)(x - x1) || (int)(y - y1))
-//	//{
-//	//	//mlx_pixel_put(d->mlx.ptr, d->mlx.win_ptr, x, y, 255);
-//	//	x += x_step;
-//	//	y += y_step;
-//	//}
+//	settings(d, &x, &y, &x1, &y1);
+//	x_step = ft_abs(x1 - x);
+//	y_step = ft_abs(y1 - y);
+//	max = ft_max(x_step, y_step);
+//	x_step /= max;
+//	y_step /= max;
+//	while ((int)(x - x1) || (int)(y - y1))
+//	{
+//		mlx_pixel_put(d->mx.init, d->mx.win, x, y, d->m.color_code);
+//		x += x_step;
+//		y += y_step;
+//	}
 //}
-
-int	deal_key(int key, void *d)
-{
-	ft_printf("%d", key);
-	return (0);
-}
-
-//void	fdf_rendering(t_data *d, t_map *m)
-//{
-//	float	x;
-//	float	y;
 //
+//int	deal_key(int key, void *d)
+//{
+//	ft_printf("%d", key);
+//	return (0);
+//}
+//
+//void	draw_bkg(t_mlx *mx)
+//{
+//	int	i;
+//	int	j;
+//	int	*img;
+//
+//	ft_bzero(mx->data_addr, WIDTH * HEIGHT * mx->bpp / 8);
+//	img = (int *)mx->data_addr;
+//	i = -1;
+//	while (++i < HEIGHT * WIDTH)
+//	{
+//		j = i % WIDTH;
+//		if (j < MENU_WIDTH)
+//			img[j] = MENU_COLOR;
+//		else
+//			img[j] = BKG_COLOR;
+//	}
+//}
+//
+//void	fdf_rendering(t_data *d, t_map *m, t_mlx *mx)
+//{
+//	double	x;
+//	double	y;
+//
+//	//draw_bkg(mx);
 //	y = 0;
-//	while (y < d->m.height)
+//	while (y < m->height)
 //	{
 //		x = 0;
-//		while (x < d->m.width)
+//		while (x < m->width)
 //		{
-//			if (x < d->m.width - 1)
+//			if (x < m->width - 1)
 //				bresenham_algo(d, x, y, x + 1, y);
-//			//if (y < d->m.height - 1)
-//			//	bresenham_algo(d, x, y, x, y + 1);
+//			if (y < m->height - 1)
+//				bresenham_algo(d, x, y, x, y + 1);
 //			x++;
 //		}
 //		y++;
 //	}
+//	//mlx_put_image_to_window(mx->init, mx->win, mx->img, 0, 0);
 //}
-
-//void	draw_map(t_data *d)
+//
+//void	draw_map(t_data *d, t_mlx *mlx)
 //{
-//	//d->mlx.ptr = mlx_init();
-//	//d->mlx.win_ptr = mlx_new_window(d->mlx.ptr, 1000, 1000, "FdF");
-//	fdf_rendering(d, &d->m);
-//	//mlx_key_hook(d->mlx.win_ptr, deal_key, NULL);
-//	//mlx_loop(d->mlx.ptr);
+//	mlx->init = mlx_init();
+//	mlx->win = mlx_new_window(mlx->init, SCR_WIDTH, SCR_HEIGHT, "FdF");
+//	mlx->img = mlx_new_image(mlx->init, SCR_WIDTH, SCR_HEIGHT);
+//	mlx->data_addr = mlx_get_data_addr(mlx->img, &mlx->bpp, &mlx->size_line, & mlx->endian);
+//	put_pixel(mlx, )
+//	fdf_rendering(d, &d->m, mlx);
+//	//mlx_key_hook(d->mx.win_ptr, deal_key, NULL);
+//	mlx_loop(mlx->init);
 //}
 
-void	init_map_data(t_map *m)
+void	init_s_matrix(t_matrix *mx)
 {
-	m->infocnt = 0;
-	m->elem = NULL;
-	m->width = 0;
-	m->height = 0;
-	m->zoom = 20;
-	m->allocsize = 0;
-	m->line = NULL;
-	m->coord = NULL;
-	m->color = NULL;
-	m->info = NULL;
+	mx->width = 0;
+	mx->height = 0;
+	mx->coord_infocnt = 0;
+	mx->elem = NULL;
+	mx->line = NULL;
+	mx->coord = NULL;
+	mx->color_code = NULL;
+	mx->coord_info = NULL;
+}
+
+t_data	*alloc_s_data(void)
+{
+	t_data	*d;
+
+	d = (t_data *)malloc(sizeof(t_data));
+	if (d)
+	{
+		d->mx = (t_matrix *)malloc(sizeof(t_matrix));
+		//d->map = (t_map *)malloc(sizeof(t_map));
+		d->mlx = (t_mlx *)malloc(sizeof(t_mlx));
+	}
+	if (!d || !d->mx || !d->mlx)
+	{
+		free_all_struct(d);
+		ft_dprintf(2, "Malloc Failure\n");
+		exit(EXIT_FAILURE);
+	}
+	return (d);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_data	d;
+	t_data		*d;
 
-	init_map_data(&d.m);
-	check_argc(&d, argc);
-	get_map(&d, argv[1]);
+	d = alloc_s_data();
+	init_s_matrix(d->mx);
+	check_argc(d, argc);
+	get_matrix(d, argv[1]);
+	//draw_map(d);
 	//int i = -1;
-	//while (++i < d.m.height)
+	//while (++i < d->mx->height)
 	//{
 	//	int j = -1;
-	//	while (++j < d.m.width)
-	//		printf("%3d",d.m.coord[i][j]);
-	//	printf("\n");
+	//	while (++j < d->mx->width)
+	//		ft_printf("%2d ", d->mx->coord[i][j]);
+	//	ft_printf("\n");
 	//}
-	draw_map(&d);
-	fdf_exit(&d, &d.m, NULL);
+	fdf_exit(d, NULL);
 }
